6d98154e5673003838f86fe8238335947c034664
Slavi123456 <slavizlatinov@gmail.com>
2024-04-28 23:31:57 +0300

    Finalize week 3

--- Week3/ForHome/Task1.hs
+++ Week3/ForHome/Task1.hs
@@ -1,7 +1,7 @@
 main::IO()
 main = do
     print $ removeFirstOccurrence 16366 5 == 16366
-    print $ removeFirstOccurrence 110 1 == 10
+    print $ removeFirstOccurrence 110 1  == 10
     print $ removeFirstOccurrence 15365 5 == 1536
     print $ removeFirstOccurrence 15360 0 == 1536
     print $ removeFirstOccurrence 15300 0 == 1530
@@ -12,10 +12,24 @@ main = do
     print $ removeFirstOccurrence (removeFirstOccurrence 1212 1) 1 == 22
 
 removeFirstOccurrence:: Int -> Int -> Int
-removeFirstOccurrence num x = reverse $ helper num 0
+removeFirstOccurrence num x
+    | helper num > len num = num
+    | otherwise = mod num (10 ^ (helper num - 1)) + (remDigit num $ helper num) 
     where
-        helper:: Int -> Int -> Int
-        helper n res 
-            | n < 10 = res * 10 + n
-            | mod n 10 == x = res
-            | otherwise = helper (mod n 10) (res * 10 + (mod n 10))
+        helper:: Int -> Int
+        helper 0 = 1
+        helper n
+            | mod n 10 == x = 1
+            | otherwise = (+1) $ helper $ div n 10
+
+remDigit::Int -> Int -> Int
+remDigit num idx =  (* (10 ^ (idx - 1))) $ div num $ (^) 10 idx
+
+len::Int -> Int 
+len x 
+    | x < 10 = 1
+    | otherwise = (+1) $ len $ div x 10
+
+-- Description:
+
+-- Define a function that removes the first occurrence of a digit in a number by going from right to left.
---
+++ Week3/ForHome/tempCodeRunnerFile.hs
@@ -0,0 +1,14 @@
+print $ sortN 1714 == 7411
+    print $ sortN 123450 == 543210
+    print $ sortN 123405 == 543210
+    print $ sortN 123045 == 543210
+    print $ sortN 120345 == 543210
+    print $ sortN 102345 == 543210
+    print $ sortN 8910 == 9810
+    print $ sortN 321 == 321
+    print $ sortN 29210 == 92210
+    print $ sortN 1230 == 3210
+    print $ sortN 55345 == 55543
+    print $ sortN 14752 == 75421
+    print $ sortN 329450 == 954320
+    print $ sortN 9125 == 9521
